e88d7e51601ef7e7f27562838d11fe70eae134e3
Lucas Oristanio Faes <142420909+lucas-oristanio@users.noreply.github.com>
2025-05-24 14:27:05 -0300

    Adiciona teste da service (#1)

--- src/test/java/com/example/demo/service/CalculadoraServiceTest.java
+++ src/test/java/com/example/demo/service/CalculadoraServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -7,20 +8,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculadoraServiceTest {
-    private final CalculadoraService service = new CalculadoraService();
-
     @Test
-    void deveDividirCorretamente() {
-        double resultado = service.dividir(10, 2);
-        assertEquals(5.0, resultado);
-    }
+    @DisplayName("Quando acionado com 10 e 0, então deve lançar uma exceção")
+    public void testDividirPorZero() {
+        CalculadoraService calculadoraService = new CalculadoraService();
+        double a = 10;
+        double b = 0;
+        var expectedMessage = "400 BAD_REQUEST \"Divisão por zero não permitida\"";
 
-    @Test
-    void deveLancarExcecaoAoDividirPorZero() {
-        ResponseStatusException excecao = assertThrows(
-                ResponseStatusException.class,
-                () -> service.dividir(10, 0)
+        ResponseStatusException exception = assertThrows(
+                ResponseStatusException.class, () -> {
+                    calculadoraService.dividir(a, b);
+                }
         );
-        assertEquals("400 BAD_REQUEST \"Divisão por zero não permitida\"", excecao.getMessage());
+
+        assertEquals(expectedMessage, exception.getMessage());
     }
 }
